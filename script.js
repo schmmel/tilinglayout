debf722e12b4931cc9666e269c52c4911e6b8504
schimmel <schmmel@users.noreply.github.com>
2025-09-11 15:13:30 +0200

    json template

---
+++ script.js
@@ -0,0 +1,20 @@
+let layoutConfig = {
+    component: [{
+        type: 'container',
+        component: [{
+            type: 'window',
+            content: 'this is a window',
+        }],
+        component: [{
+            type: 'container',
+            component: [{
+                type: 'window',
+                content: 'this is also a window but smaller',
+            }],
+            component: [{
+                type: 'window',
+                content: 'test'
+            }]
+        }]
+    }]
+}
