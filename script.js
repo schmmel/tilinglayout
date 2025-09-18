c42cdfa5e0bb6d8d7d473b51bc338d9cd7789b1d
schimmel <schmmel@users.noreply.github.com>
2025-09-18 14:00:16 +0200

    json parsing

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
 let layoutConfig = {
-    component: [{
+    layoutContainer: {
         type: 'container',
-        component: [{
-            type: 'window',
-            content: 'this is a window',
-        }],
-        component: [{
-            type: 'container',
-            component: [{
+        content: [
+            {
                 type: 'window',
-                content: 'this is also a window but smaller',
-            }],
-            component: [{
-                type: 'window',
-                content: 'test'
-            }]
-        }]
-    }]
+                content: 'this is a window',
+            },
+            {
+                type: 'container',
+                content: [
+                    {
+                        type: 'window',
+                        content: 'this is also a window but smaller',
+                    },
+                    {
+                        type: 'window',
+                        content: 'this is also a window but smaller 2',
+                    }
+                ]
+            }
+        ]
+    }
 }
+
+let layout = {
+    container: document.querySelector('#layoutContainer'),
+    width: 0,
+    height: 0,
+    orientation: '',
+}
+
+clientParameters();
+
+
+
+window.addEventListener('resize', () => {
+    clientParameters();
+});
+
+function clientParameters() {
+    layout.width = layout.container.clientWidth;
+    layout.height = layout.container.clientHeight;
+
+    layout.orientation = layout.width >= layout.height ? 'horizontal' : 'vertical';
+}
+
+function loadWindows() {
+    const stack = [layoutConfig];
+    while (stack.length > 0) {
+        const currentObject = stack.pop();
+        Object.keys(currentObject).forEach(key => {
+            if (typeof currentObject[key] === 'object') {
+                if (Object.prototype.toString.apply(currentObject[key]) === '[object Object]') {
+                    console.log(currentObject[key]);
+                }
+                stack.push(currentObject[key]);
+            }
+        });
+    }
+}
+
+loadWindows();
