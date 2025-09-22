761aaf1e2ab8d5cc4227864e936212baa9684e58
schimmel <schmmel@users.noreply.github.com>
2025-09-22 19:39:22 +0200

    it tiles

---
+++ config.js
@@ -0,0 +1,40 @@
+let layoutConfig = {
+    "layoutContainer": {
+        "type": "container",
+        "id": "0",
+        "content": [
+            {
+                "type": "window",
+                "id": "00",
+                "content": "this is a window"
+            },
+            {
+                "type": "container",
+                "id": "01",
+                "content": [
+                    {
+                        "type": "window",
+                        "id": "010",
+                        "content": "this is also a window but smaller"
+                    },
+                    {
+                        "type": "container",
+                        "id": "011",
+                        "content": [
+                            {
+                                "type": "window",
+                                "id": "0110",
+                                "content": "this is also a window but even smaller"
+                            },
+                            {
+                                "type": "window",
+                                "id": "0111",
+                                "content": "this is also a window but even smaller 2"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
--- script.js
+++ script.js
@@ -1,91 +1,70 @@
-let layoutConfig = {
-    layoutContainer: {
-        type: 'container',
-        id: '0',
-        content: [
-            {
-                type: 'window',
-                id: '00',
-                content: 'this is a window',
-            },
-            {
-                type: 'container',
-                id: '01',
-                content: [
-                    {
-                        type: 'container',
-                        id: '010',
-                        content: [
-                            {
-                                type: 'window',
-                                id: '0100',                                
-                                content: 'this is also a window but smaller',
-                            },
-                            {
-                                type: 'window',
-                                id: '0101',                                
-                                content: 'this is also a window but smaller 2',
-                            }
-                        ]
-                    },
-                    {
-                        type: 'container',
-                        id: '011',
-                        content: [
-                            {
-                                type: 'window',
-                                id: '0110',
-                                content: 'this is also a window but smaller 3',
-                            },
-                            {
-                                type: 'window',
-                                id: '0111',
-                                content: 'this is also a window but smaller 4',
-                            }
-                        ]
-                    },
-
-                ]
-            }
-        ]
-    }
-}
-
 let layout = {
     container: document.querySelector('#layoutContainer'),
     width: 0,
     height: 0,
-    orientation: '',
+    primaryOrientation: '',
+    secondaryOrientation: '',
 }
 
 clientParameters();
 
-
+createComponents(readJSON());
 
 window.addEventListener('resize', () => {
     clientParameters();
+    createComponents(readJSON());
 });
 
 function clientParameters() {
     layout.width = layout.container.clientWidth;
     layout.height = layout.container.clientHeight;
 
-    layout.orientation = layout.width >= layout.height ? 'horizontal' : 'vertical';
+    layout.primaryOrientation = layout.width >= layout.height ? 'horizontal' : 'vertical';
+    layout.secondaryOrientation = layout.width >= layout.height ? 'vertical' : 'horizontal';
 }
 
-function loadWindows() {
+function readJSON() {
+    let components = [];
+
     const stack = [layoutConfig];
     while (stack.length > 0) {
         const currentObject = stack.pop();
         Object.keys(currentObject).forEach(key => {
             if (typeof currentObject[key] === 'object') {
                 if (Object.prototype.toString.apply(currentObject[key]) === '[object Object]') {
-                    console.log(currentObject[key]);
+                    components.push(currentObject[key]);
                 }
                 stack.push(currentObject[key]);
             }
         });
     }
+
+    return components;
 }
 
-loadWindows();
+function createComponents(components) {
+    layout.container.innerHTML = "";
+
+    components.forEach(componentObject => {
+        const component = document.createElement("div");
+        component.id = componentObject.id;
+        component.className = componentObject.type;
+
+        if (componentObject.id.length % 2) {
+            component.className += " " + layout.primaryOrientation;
+        } else {
+            component.className += " " + layout.secondaryOrientation;
+        }
+
+        if (componentObject.type == "window") {
+            component.innerHTML = componentObject.content;
+        }
+
+        if (componentObject.id == "0") {
+            layout.container.appendChild(component);
+            return;
+        }
+        
+        document.getElementById(componentObject.id.slice(0, -1)).appendChild(component);
+    });
+}
