9a9a573cf286ab1e0246e1e4008843cc953527f7
schimmel <schmmel@users.noreply.github.com>
2025-11-26 01:50:11 +0100

    unified formatting

--- script.js
+++ script.js
@@ -5,26 +5,26 @@ let layout = {
     height: 0,
     borderSize: 4, // additional to css border
     defaultContainerSize: 1,
-    latestCreatedWindow: "",
+    latestCreatedWindow: '',
     tempI: 0,
 }
 
 
-const containerConfig = ["c0", "c00", "c01"];
+const containerConfig = ['c0', 'c00', 'c01'];
 const windowConfig = {
-    "w00": "\n<a href=\"#\" onclick=\"createWindow(layout.latestCreatedWindow, 1, 'created window ' + layout.tempI)\">create window</a>",
-    "w01": "this is the window content",
+    'w00': '\n<a href="#" onclick="createWindow(layout.latestCreatedWindow, 1, "created window " + layout.tempI)">create window</a>',
+    'w01': 'this is the window content',
 };
 const windowTitles = {
-    "w00": "TESTING WINDOW",
-    "w01": "BIG WINDOW",
+    'w00': 'TESTING WINDOW',
+    'w01': 'BIG WINDOW',
 }
 
 let containers = [];
 
 let containerSizes = {
-    "c00": (1 / 3) * layout.defaultContainerSize,
-    "c01": (2 / 3) * layout.defaultContainerSize,
+    'c00': (1 / 3) * layout.defaultContainerSize,
+    'c01': (2 / 3) * layout.defaultContainerSize,
 }
 
 loadContainerConfig(containerConfig);
@@ -34,12 +34,12 @@ clientParameters();
 // for testing only
 // layout.root.addEventListener('mousedown', (e) => {
 //     if (e.button == 0) {
-//         createWindow(e.target.id, 0, "new window");
+//         createWindow(e.target.id, 0, 'new window');
 //     } else if (e.button == 2) {
 //         destroyWindow(e.target.id);
 //     }
 // });
-// window.addEventListener("contextmenu", (e) => {
+// window.addEventListener('contextmenu', (e) => {
 //     e.preventDefault();
 // });
 
@@ -56,8 +56,8 @@ function clientParameters() {
 }
 
 function setContainerFlexDirection() {
-    const primary = [...document.getElementsByClassName("primaryOrientation")];
-    const secondary = [...document.getElementsByClassName("secondaryOrientation")];
+    const primary = [...document.getElementsByClassName('primaryOrientation')];
+    const secondary = [...document.getElementsByClassName('secondaryOrientation')];
 
     primary.forEach(element => {
         element.style.flexDirection = layout.direction === 'horizontal' ? 'column' : 'row';
@@ -73,20 +73,20 @@ function setContainerFlexDirection() {
 // element id is always 1 longer than parent element
 function setContainerClass(element) {
     if (element.id.length % 2) {
-        element.classList.remove("secondaryOrientation");
-        element.classList.add("primaryOrientation");
+        element.classList.remove('secondaryOrientation');
+        element.classList.add('primaryOrientation');
     } else {
-        element.classList.remove("primaryOrientation");
-        element.classList.add("secondaryOrientation");
+        element.classList.remove('primaryOrientation');
+        element.classList.add('secondaryOrientation');
     }
 }
 
 function loadContainerConfig(containerConfig) {
     containerConfig.forEach(container => {
 
-        const element = document.createElement("div");
+        const element = document.createElement('div');
         element.id = container;
-        element.className = "container";
+        element.className = 'container';
 
         setContainerClass(element);
 
@@ -95,7 +95,7 @@ function loadContainerConfig(containerConfig) {
             containerSizes[container] = layout.defaultContainerSize
         }
 
-        if (container == "c0") {
+        if (container == 'c0') {
             layout.root.appendChild(element);
             return;
         }
@@ -108,23 +108,23 @@ function loadContainerConfig(containerConfig) {
 
 function loadWindowConfig(windowConfig) {
     Object.keys(windowConfig).forEach(windowId => {
-        const element = document.createElement("div");
+        const element = document.createElement('div');
         element.id = windowId;
-        element.className = "window";
+        element.className = 'window';
 
-        createComponent(element, "header", windowTitles[windowId]);
-        createComponent(element, "content", windowConfig[windowId]);
+        createComponent(element, 'header', windowTitles[windowId]);
+        createComponent(element, 'content', windowConfig[windowId]);
 
         document.getElementById('c' + windowId.slice(1)).appendChild(element);
 
-        element.addEventListener("mousedown", resizeListener);
+        element.addEventListener('mousedown', resizeListener);
     })
 }
 
 function createWindow(targetId, newWindowLocation, content) {
     layout.tempI++
 
-    if (targetId === "") {
+    if (targetId === '') {
         targetId = 'w' + containers[containers.length - 1].slice(1);
     }
 
@@ -133,9 +133,9 @@ function createWindow(targetId, newWindowLocation, content) {
 
     // create 2 new containers
     for (let i = 0; i < 2; i++) {
-        const element = document.createElement("div");
+        const element = document.createElement('div');
         element.id = parentContainer + i.toString();
-        element.className = "container";
+        element.className = 'container';
 
         if (containerSizes[element.id] == undefined) {
             containerSizes[element.id] = layout.defaultContainerSize;
@@ -147,17 +147,17 @@ function createWindow(targetId, newWindowLocation, content) {
     }
 
     // create new window and append to specified container
-    const element = document.createElement("div");
+    const element = document.createElement('div');
     element.id = 'w' + parentContainer.slice(1) + newWindowLocation.toString();
-    element.className = "window";
+    element.className = 'window';
 
-    createComponent(element, "header", content);
-    createComponent(element, "content", content);
+    createComponent(element, 'header', content);
+    createComponent(element, 'content', content);
 
     document.getElementById('c' + element.id.slice(1)).appendChild(element);
     layout.latestCreatedWindow = element.id;
 
-    element.addEventListener("mousedown", resizeListener);
+    element.addEventListener('mousedown', resizeListener);
 
     // append original window to opposite container
     const oldWindowLocation = newWindowLocation ? 0 : 1;
@@ -171,12 +171,12 @@ function createWindow(targetId, newWindowLocation, content) {
 
 function destroyWindow(targetId) {
     // dont destroy window if its the only window
-    if (targetId == "w0") {
+    if (targetId == 'w0') {
         return;
     }
 
     if (targetId === layout.latestCreatedWindow) {
-        layout.latestCreatedWindow = "";
+        layout.latestCreatedWindow = '';
     }
 
     const target = document.getElementById(targetId);
@@ -192,7 +192,7 @@ function destroyWindow(targetId) {
 }
 
 function reformatWindows(target) {
-    const elements = [...target.getElementsByTagName("*")];
+    const elements = [...target.getElementsByTagName('*')];
     const nthDigitToRemove = target.id.length;
 
     for (let i = 0; i < elements.length; i++) {
@@ -205,13 +205,13 @@ function reformatWindows(target) {
                 layout.root.appendChild(element);
             } else {
                 document.getElementById(element.id.slice(0, -2)).appendChild(element);
-                document.getElementById(element.id.slice(0, -2)).appendChild(document.getElementById(element.id.slice(0, -2) + "1"));
+                document.getElementById(element.id.slice(0, -2)).appendChild(document.getElementById(element.id.slice(0, -2) + '1'));
             }
 
             target.remove();
         };
 
-        if (element.classList.contains("window")) {
+        if (element.classList.contains('window')) {
             // element.id = element.id.slice(0, -1);
 
             element.id = element.id.slice(0, nthDigitToRemove) + element.id.slice(nthDigitToRemove + 1);
@@ -220,7 +220,7 @@ function reformatWindows(target) {
                 layout.latestCreatedWindow = element.id;
             }
 
-        } else if (element.classList.contains("container")) {
+        } else if (element.classList.contains('container')) {
             containerSizes[element.id.slice(0, nthDigitToRemove) + element.id.slice(nthDigitToRemove + 1)] = containerSizes[element.id];
             delete containerSizes[element.id];
 
@@ -239,7 +239,7 @@ let resizeTarget;
 let resizeDirection;
 
 function resizeListener(e) {
-    if (!e.target.classList.contains("window")) {
+    if (!e.target.classList.contains('window')) {
         return;
     }
 
@@ -253,43 +253,54 @@ function resizeListener(e) {
     }
 
     if (e.offsetX < layout.borderSize) {
-        resizeDirection = "left";
+        resizeDirection = 'left';
     } else if (e.offsetX > e.target.clientWidth - layout.borderSize) {
-        resizeDirection = "right";
+        resizeDirection = 'right';
     } else if (e.offsetY < layout.borderSize) {
-        resizeDirection = "top";
+        resizeDirection = 'top';
     } else if (e.offsetY > e.target.clientHeight - layout.borderSize) {
-        resizeDirection = "bottom";
+        resizeDirection = 'bottom';
     } else {
         return;
     }
 
     resizeTarget = e.target;
-    document.addEventListener("mousemove", resizeWindow);
+    document.addEventListener('mousemove', resizeWindow);
 }
 
 function resizeWindow(e) {
 
     // detect window on opposite side of border
+    let primaryTarget = resizeTarget.id;
     let secondaryTarget;
     switch (resizeDirection) {
-        case "left":
-            secondaryTarget = document.elementFromPoint(resizeTarget.offsetLeft - 1, e.y);
+        case 'left':
+            secondaryTarget = document.elementFromPoint(resizeTarget.offsetLeft - 1, e.y).id;
             break;
-        case "right":
-            secondaryTarget = document.elementFromPoint(resizeTarget.offsetLeft + resizeTarget.offsetWidth + 1, e.y);
+        case 'right':
+            secondaryTarget = document.elementFromPoint(resizeTarget.offsetLeft + resizeTarget.offsetWidth + 1, e.y).id;
             break;
-        case "top":
-            secondaryTarget = document.elementFromPoint(e.x, resizeTarget.offsetTop - 1);
+        case 'top':
+            secondaryTarget = document.elementFromPoint(e.x, resizeTarget.offsetTop - 1).id;
             break;
-        case "bottom":
-            secondaryTarget = document.elementFromPoint(e.x, resizeTarget.offsetTop + resizeTarget.offsetHeight + 1);
+        case 'bottom':
+            secondaryTarget = document.elementFromPoint(e.x, resizeTarget.offsetTop + resizeTarget.offsetHeight + 1).id;
             break;
     }
+
+    let sharedParent;
+    for (let i = 0; i < Math.min(primaryTarget.length, secondaryTarget.length); i++) {
+        if (primaryTarget[i] !== secondaryTarget[i]) {
+            sharedParent = 'c' + primaryTarget.slice(1, i - primaryTarget.length);
+            break;
+        }
+    }
+
+    console.log(sharedParent);
 }
 
 document.addEventListener('mouseup', () => {
-    document.removeEventListener("mousemove", resizeWindow);
+    document.removeEventListener('mousemove', resizeWindow);
 });
 
 function setContainerSize() {
@@ -297,9 +308,9 @@ function setContainerSize() {
         const targetContainer = document.getElementById(key);
 
         // container 0 has no siblings
-        if (key == "c0") {
-            targetContainer.style.height = "100%";
-            targetContainer.style.width = "100%";
+        if (key == 'c0') {
+            targetContainer.style.height = '100%';
+            targetContainer.style.width = '100%';
             return;
         }
 
@@ -308,21 +319,21 @@ function setContainerSize() {
 
         // hacky fix for some containers not having corresponding sizes???
         // i dont know why they dont have corresponding sizes but they should
-        if (containerSizes[parentContainer + "0"] == undefined) {
-            containerSizes[parentContainer + "0"] = layout.defaultContainerSize;
+        if (containerSizes[parentContainer + '0'] == undefined) {
+            containerSizes[parentContainer + '0'] = layout.defaultContainerSize;
         }
-        if (containerSizes[parentContainer + "1"] == undefined) {
-            containerSizes[parentContainer + "1"] = layout.defaultContainerSize;
+        if (containerSizes[parentContainer + '1'] == undefined) {
+            containerSizes[parentContainer + '1'] = layout.defaultContainerSize;
         }
 
-        const totalSize = containerSizes[parentContainer + "0"] + containerSizes[parentContainer + "1"];
+        const totalSize = containerSizes[parentContainer + '0'] + containerSizes[parentContainer + '1'];
 
-        if (targetContainer.style.flexDirection == "row") {
-            targetContainer.style.height = ((containerSize / totalSize) * 100) + "%";
-            targetContainer.style.width = "100%";
-        } else if (targetContainer.style.flexDirection == "column") {
-            targetContainer.style.height = "100%";
-            targetContainer.style.width = ((containerSize / totalSize) * 100) + "%";
+        if (targetContainer.style.flexDirection == 'row') {
+            targetContainer.style.height = ((containerSize / totalSize) * 100) + '%';
+            targetContainer.style.width = '100%';
+        } else if (targetContainer.style.flexDirection == 'column') {
+            targetContainer.style.height = '100%';
+            targetContainer.style.width = ((containerSize / totalSize) * 100) + '%';
         }
     })
 }
@@ -330,7 +341,7 @@ function setContainerSize() {
 function populateContainersArray() {
     containers = [];
 
-    const existingContainers = [...document.getElementsByClassName("container")];
+    const existingContainers = [...document.getElementsByClassName('container')];
 
     existingContainers.forEach(container => {
         containers.push(container.id)
@@ -341,30 +352,30 @@ function createComponent(target, componentType, content) {
     let component = null;
 
     switch (componentType) {
-        case "header":
-            component = document.createElement("div");
-            component.className = "header";
+        case 'header':
+            component = document.createElement('div');
+            component.className = 'header';
             target.appendChild(component);
-            createComponent(component, "title", content)
-            createComponent(component, "close-button");
+            createComponent(component, 'title', content)
+            createComponent(component, 'close-button');
             break;
-        case "title":
-            component = document.createElement("span");
-            component.className = "title";
+        case 'title':
+            component = document.createElement('span');
+            component.className = 'title';
             component.innerText = content.toUpperCase();
             target.appendChild(component);
             break;
-        case "close-button":
-            component = document.createElement("button");
-            component.type = "button";
-            component.className = "close-button";
-            component.textContent = "X";
-            component.setAttribute("onclick", "destroyWindow(this.parentElement.parentElement.id)");
+        case 'close-button':
+            component = document.createElement('button');
+            component.type = 'button';
+            component.className = 'close-button';
+            component.textContent = 'X';
+            component.setAttribute('onclick', 'destroyWindow(this.parentElement.parentElement.id)');
             target.appendChild(component);
             break;
-        case "content":
-            component = document.createElement("div");
-            component.className = "content";
+        case 'content':
+            component = document.createElement('div');
+            component.className = 'content';
             component.innerHTML = content;
             target.appendChild(component);
             break;
